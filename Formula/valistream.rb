3a404803f952c821e1346c132c0384b5e1d32150
volodymyrai <51513546+volodymyrai@users.noreply.github.com>
2026-06-17 11:08:10 +0200

    Added homebrew formula

---
+++ Formula/valistream.rb
@@ -0,0 +1,17 @@
+class Valistream < Formula
+  desc "Validates and monitors HLS playlists (live & vod) against RFC 8216 and Apple HLS authoring rules."
+  homepage "https://github.com/volodymyrai/homebrew-valistream"
+  url "https://github.com/volodymyrai/homebrew-valistream/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "sha256:38b121430f1ed974f391e8006057876c5247011e98f91e8bc9fdc932666a2ad9"
+  license "MIT"
+
+  # For pre-compiled native binaries
+  def install
+    bin.install "valistream" => "valistream"
+  end
+
+  test do
+    # Simple test ensuring the binary executes and doesn't return an error
+    system "#{bin}/valistream", "--version"
+  end
+end
