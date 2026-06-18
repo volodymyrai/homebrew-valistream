135c8d7bfe1660d8aaa75668d28e7c7f73d428bd
volodymyrai <51513546+volodymyrai@users.noreply.github.com>
2026-06-18 15:09:18 +0200

    Update Valistream version to 0.6.0

--- Formula/valistream.rb
+++ Formula/valistream.rb
@@ -1,8 +1,8 @@
 class Valistream < Formula
   desc "Validates and monitors HLS playlists (live & vod) against RFC 8216 and Apple HLS authoring rules."
   homepage "https://github.com/volodymyrai/valistream"
-  url "https://github.com/volodymyrai/valistream/releases/download/v0.5.0/valistream.zip"
-  sha256 "38b121430f1ed974f391e8006057876c5247011e98f91e8bc9fdc932666a2ad9"
+  url "https://github.com/volodymyrai/valistream/releases/download/v0.6.0/valistream.zip"
+  sha256 "3ab508873bade50ad854a770eb0391ca2d3d4da5e99c69af1bc3428eef56fd0b"
   license "MIT"
 
   # For pre-compiled native binaries
